c8c7d8e84444ad8d6257b4b31ef01e8c1228aa8b
Andy960606 <129306977+Andy960606@users.noreply.github.com>
2023-03-30 10:18:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const apiKey = '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'; // Replace with your OpenAI API key
-const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-
-function generate() {
-  const input = document.getElementById('input').value;
-  const output = document.getElementById('output');
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  headers.append('Authorization', 'Bearer ' + apiKey);
-  const body = {
-    prompt: input,
-    max_tokens: 60,
-    temperature: 0.7,
-    n: 1,
-    stop: '\n'
-  };
-  fetch(apiUrl, {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(body)
-  })
-  .then(response => response.json())
-  .then(data => {
-    const result = data.choices[0].text.trim();
-    output.value = result;
-  })
-  .catch(error => console.error(error));
-}
+// 在文档加载后运行脚本
+document.addEventListener("DOMContentLoaded", function(event) {
+    // 替换为您的OpenAI API密钥
+    const apiKey = "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";
+    
+    // 创建OpenAI API客户端
+    const openai = new OpenAI(apiKey);
+    
+    // 获取文本输入和输出元素
+    const inputElement = document.getElementById("input");
+    const outputElement = document.getElementById("output");
+    
+    // 监听文本输入元素
+    inputElement.addEventListener("input", async function(event) {
+        // 获取输入文本
+        const inputText = event.target.value;
+        
+        try {
+            // 同步OpenAI API
+            const response = await openai.complete({
+                engine: "davinci",
+                prompt: inputText,
+                maxTokens: 2048,
+                temperature: 0.5
+            });
+            
+            // 显示输出文本
+            outputElement.value = response.data.choices[0].text;
+        } catch(error) {
+            // 显示错误消息
+            outputElement.value = "Error: " + error.message;
+        }
+    });
+});
