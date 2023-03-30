2dd083b1032730eedaa183db361b0c94393f20d3
Andy960606 <129306977+Andy960606@users.noreply.github.com>
2023-03-30 20:38:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const apiKey = process.env.API_KEY;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Define a function to handle the form submission
+async function handleSubmit(event) {
+	event.preventDefault();
+
+	// Get the user input from the form
+	const userInput = document.getElementById("userInput").value;
+
+	// Send a request to the OpenAI API
+	const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${apiKey}`
+		},
+		body: JSON.stringify({
+			prompt: userInput,
+			max_tokens: 60,
+			n: 1,
+			stop: ["\n"]
+		})
+	});
+
+	// Get the response from the API
+	const data = await response.json();
+
+	// Display the response on the page
+	const outputElement = document.getElementById("output");
+	outputElement.innerHTML += `<p><strong>You:</strong> ${userInput}</p>`;
+	outputElement.innerHTML += `<p><strong>AI:</strong> ${data.choices[0].text}</p>`;
+}
+
+// Add an event listener to the form
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
